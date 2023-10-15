eb6e3281ec50316e032d8da9c938b2c78149c7c7
siiisiiiii <144625172+siiisiiiii@users.noreply.github.com>
2023-10-15 13:33:38 +0300

    Delete Lab2BaranKI306 directory

--- Lab2BaranKI306/Lab2BaranKI306/Lab2Doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"App"},{"p":"org.example","l":"Brain"},{"p":"org.example","l":"Mouth"},{"p":"org.example","l":"Person"}];updateSearchResults();
